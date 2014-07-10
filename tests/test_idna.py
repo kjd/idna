edce74a9661957b71f5d6c9b095592643bb9905d
Kim Davies <kim@cynosure.com.au>
2014-07-09 19:05:05 -0700

    Tests for issue #3

--- tests/test_idna.py
+++ tests/test_idna.py
@@ -241,6 +241,10 @@ class IDNATests(unittest.TestCase):
         self.assertEqual(idna.encode('abc.abc'), b'abc.abc')
         self.assertEqual(idna.encode('xn--zckzah.abc'), b'xn--zckzah.abc')
         self.assertEqual(idna.encode(u'\u30c6\u30b9\u30c8.abc'), b'xn--zckzah.abc')
+        self.assertEqual(idna.encode(u'\u0521\u0525\u0523-\u0523\u0523-----\u0521\u0523\u0523\u0523.aa'),
+                                     b'xn---------90gglbagaar.aa')
+        self.assertRaises(idna.IDNAError, idna.encode,
+                          u'\u0521\u0524\u0523-\u0523\u0523-----\u0521\u0523\u0523\u0523.aa')
 
     def test_decode(self):
 
@@ -249,7 +253,10 @@ class IDNATests(unittest.TestCase):
         self.assertEqual(idna.decode(u'\u30c6\u30b9\u30c8.\u30c6\u30b9\u30c8'),
                          u'\u30c6\u30b9\u30c8.\u30c6\u30b9\u30c8')
         self.assertEqual(idna.decode('abc.abc'), u'abc.abc')
-
+        self.assertEqual(idna.decode('xn---------90gglbagaar.aa'),
+                                     u'\u0521\u0525\u0523-\u0523\u0523-----\u0521\u0523\u0523\u0523.aa')
+        self.assertRaises(idna.IDNAError, idna.decode, 'XN---------90GGLBAGAAC.AA')
+        self.assertRaises(idna.IDNAError, idna.decode, 'xn---------90gglbagaac.aa')
 
 if __name__ == '__main__':
     unittest.main()
