6684767d54d23e66ffa6bbaedaee939f08536704
Kim Davies <kim.davies@icann.org>
2014-07-14 14:07:08 -0700

    Codec tests for Python 3

--- tests/test_idna_codec.py
+++ tests/test_idna_codec.py
@@ -17,28 +17,31 @@ class IDNACodecTests(unittest.TestCase):
         # Tests derived from Python standard library test/test_codecs.py
 
         incremental_tests = (
-            (u"python.org", "python.org"),
-            (u"python.org.", "python.org."),
-            (u"pyth\xf6n.org", "xn--pythn-mua.org"),
-            (u"pyth\xf6n.org.", "xn--pythn-mua.org."),
+            (u"python.org", b"python.org"),
+            (u"python.org.", b"python.org."),
+            (u"pyth\xf6n.org", b"xn--pythn-mua.org"),
+            (u"pyth\xf6n.org.", b"xn--pythn-mua.org."),
         )
 
         for decoded, encoded in incremental_tests:
-            self.assertEqual("".join(codecs.iterdecode(encoded, "idna")),
-                 decoded)
-
+            if sys.version_info.major == 2:
+                self.assertEqual("".join(codecs.iterdecode(encoded, "idna")),
+                                decoded)
+            else:
+                self.assertEqual("".join(codecs.iterdecode((bytes([c]) for c in encoded), "idna")),
+                                decoded)
 
         decoder = codecs.getincrementaldecoder("idna")()
-        self.assertEqual(decoder.decode("xn--xam", ), u"")
-        self.assertEqual(decoder.decode("ple-9ta.o", ), u"\xe4xample.")
-        self.assertEqual(decoder.decode(u"rg"), u"")
-        self.assertEqual(decoder.decode(u"", True), u"org")
+        self.assertEqual(decoder.decode(b"xn--xam", ), u"")
+        self.assertEqual(decoder.decode(b"ple-9ta.o", ), u"\xe4xample.")
+        self.assertEqual(decoder.decode(b"rg"), u"")
+        self.assertEqual(decoder.decode(b"", True), u"org")
 
         decoder.reset()
-        self.assertEqual(decoder.decode("xn--xam", ), u"")
-        self.assertEqual(decoder.decode("ple-9ta.o", ), u"\xe4xample.")
-        self.assertEqual(decoder.decode("rg."), u"org.")
-        self.assertEqual(decoder.decode("", True), u"")
+        self.assertEqual(decoder.decode(b"xn--xam", ), u"")
+        self.assertEqual(decoder.decode(b"ple-9ta.o", ), u"\xe4xample.")
+        self.assertEqual(decoder.decode(b"rg."), u"org.")
+        self.assertEqual(decoder.decode(b"", True), u"")
 
 
     def testIncrementalEncoder(self):
@@ -46,24 +49,24 @@ class IDNACodecTests(unittest.TestCase):
         # Tests derived from Python standard library test/test_codecs.py
 
         incremental_tests = (
-            (u"python.org", "python.org"),
-            (u"python.org.", "python.org."),
-            (u"pyth\xf6n.org", "xn--pythn-mua.org"),
-            (u"pyth\xf6n.org.", "xn--pythn-mua.org."),
+            (u"python.org", b"python.org"),
+            (u"python.org.", b"python.org."),
+            (u"pyth\xf6n.org", b"xn--pythn-mua.org"),
+            (u"pyth\xf6n.org.", b"xn--pythn-mua.org."),
         )
         for decoded, encoded in incremental_tests:
-            self.assertEqual("".join(codecs.iterencode(decoded, "idna")),
+            self.assertEqual(b"".join(codecs.iterencode(decoded, "idna")),
                              encoded)
 
         encoder = codecs.getincrementalencoder("idna")()
-        self.assertEqual(encoder.encode(u"\xe4x"), "")
-        self.assertEqual(encoder.encode(u"ample.org"), "xn--xample-9ta.")
-        self.assertEqual(encoder.encode(u"", True), "org")
+        self.assertEqual(encoder.encode(u"\xe4x"), b"")
+        self.assertEqual(encoder.encode(u"ample.org"), b"xn--xample-9ta.")
+        self.assertEqual(encoder.encode(u"", True), b"org")
 
         encoder.reset()
-        self.assertEqual(encoder.encode(u"\xe4x"), "")
-        self.assertEqual(encoder.encode(u"ample.org."), "xn--xample-9ta.org.")
-        self.assertEqual(encoder.encode(u"", True), "")
+        self.assertEqual(encoder.encode(u"\xe4x"), b"")
+        self.assertEqual(encoder.encode(u"ample.org."), b"xn--xample-9ta.org.")
+        self.assertEqual(encoder.encode(u"", True), b"")
 
 if __name__ == '__main__':
     unittest.main()
