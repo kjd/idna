5d156fc487f2683597455bb3f598057e038c7e82
Kim Davies <kim.davies@icann.org>
2014-07-14 19:49:46 -0700

    Fill out idna.compat tests

--- idna/compat.py
+++ idna/compat.py
@@ -4,10 +4,10 @@ from idna.codec import *
 dots = re.compile(u"[\u002E\u3002\uFF0E\uFF61]")
 
 def ToASCII(label):
-    return alabel(label)
+    return encode(label)
 
 def ToUnicode(label):
-    return ulabel(label)
+    return decode(label)
 
 def nameprep(s):
     raise NotImplementedError("IDNA 2008 does not utilise nameprep protocol")
--- tests/test_idna_compat.py
+++ tests/test_idna_compat.py
@@ -9,12 +9,13 @@ import idna.compat
 class IDNACompatTests(unittest.TestCase):
 
     def testToASCII(self):
-        pass
+        self.assertEqual(idna.compat.ToASCII(u'\u30c6\u30b9\u30c8.xn--zckzah'), b'xn--zckzah.xn--zckzah')
 
     def testToUnicode(self):
+        self.assertEqual(idna.compat.ToUnicode(b'xn--zckzah.xn--zckzah'), u'\u30c6\u30b9\u30c8.\u30c6\u30b9\u30c8')
         pass
 
-    def testnameprep(self):
+    def test_nameprep(self):
         self.assertRaises(NotImplementedError, idna.compat.nameprep, "a")
 
 if __name__ == '__main__':
