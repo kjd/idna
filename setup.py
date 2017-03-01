b4a95b6aa6bf117221dfc2df64accba95f3315d8
Kim Davies <kim@cynosure.com.au>
2017-02-28 20:31:01 -0800

    Update trove classifier for Python 3.6

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ def main():
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
             'Topic :: Internet :: Name Service (DNS)',
             'Topic :: Software Development :: Libraries :: Python Modules',
             'Topic :: Utilities',
