2618ad17c86498d627ebb136143c2799ad48cbe3
Jon Dufresne <jon.dufresne@gmail.com>
2018-06-10 20:05:59 -0700

    Stop testing EOL Python 2.6 and 3.3 (#62)
    Python 2.6 and 3.3 are EOL. They are no longer receiving bug fixes,
    including for security issues. Python 2.6 went EOL on 2013-10-29 and
    Python 3.3 went EOL on 2017-09-29. For details, see:
    
    https://devguide.python.org/#status-of-python-branches
    
    For additional discussion on why project maintainers may wish to drop
    Python 2.6 support, please see:
    
    https://snarky.ca/stop-using-python-2-6/
    http://www.curiousefficiency.org/posts/2015/04/stop-supporting-python26.html
    
    Reduces testing resources.

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ from setuptools import setup
 def main():
 
     python_version = sys.version_info[:2]
-    if python_version < (2,6):
-        raise SystemExit("Sorry, Python 2.6 or newer required")
+    if python_version < (2,7):
+        raise SystemExit("Sorry, Python 2.7 or newer required")
 
     package_data = {}
     exec(open('idna/package_data.py').read(), package_data)
@@ -36,10 +36,9 @@ def main():
             'License :: OSI Approved :: BSD License',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
-            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
@@ -47,6 +46,7 @@ def main():
             'Topic :: Software Development :: Libraries :: Python Modules',
             'Topic :: Utilities',
         ],
+        'python_requires': '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
         'test_suite': 'tests',
     }
 
